2d8ecd21e2e6a8874679eb10c5e34ac5b0c67b47
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-02-17 12:22:26 +0900

    Add Credentials structure and extension with preset credentials

--- MVVMSimpleLoginScreen/Sources/Models/UserDataModel.swift
+++ MVVMSimpleLoginScreen/Sources/Models/UserDataModel.swift
@@ -1,8 +1,16 @@
-//
-//  UserDataModel.swift
-//  MVVMSimpleLoginScreen
-//
-//  Created by Churkin Vitaly on 17.02.2024.
-//
-
 import Foundation
+
+// Model structure representing user credentials
+struct Credentials {
+    var username: String
+    var password: String
+}
+
+// Extension for additional functionality related to Credentials
+extension Credentials {
+    // Static array containing predefined user credentials for demonstration purposes
+    static var users: [Credentials] = [
+        Credentials(username: "admin", password: "admin"),
+        Credentials(username: "user", password: "user")
+    ]
+}
