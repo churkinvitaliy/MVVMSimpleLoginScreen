fc312871afc70f0815a1f7eaa4ad79000333fa4b
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-02-17 12:24:59 +0900

    Add authentication logic

--- MVVMSimpleLoginScreen/Sources/VeiwModels/LoginViewModel.swift
+++ MVVMSimpleLoginScreen/Sources/VeiwModels/LoginViewModel.swift
@@ -1,1 +1,34 @@
 import Foundation
+import Combine
+import UIKit
+
+class LoginViewModel {
+
+    // Published properties to observe changes in the UI
+    @Published var selectedColor: UIColor?
+    @Published var selectedText: String?
+
+    // Set to hold Combine cancellables to manage subscriptions
+    var cancellables = Set<AnyCancellable>()
+
+    // Function to authenticate the user based on provided credentials
+    func authenticate(username: String, password: String) {
+        // Check if the provided credentials are valid
+        let isValid = isValidCredentials(username: username, password: password)
+
+        // Update UI properties based on authentication result
+        if isValid {
+            selectedText = "Привет, \(username.capitalized)!"
+            selectedColor = .systemGreen
+        } else {
+            selectedText = "Неверный логин или пароль!"
+            selectedColor = .systemRed
+        }
+    }
+
+    // Private helper function to check if the credentials are valid
+    private func isValidCredentials(username: String, password: String) -> Bool {
+        // Check if the provided username and password match the credentials from the model
+        return Credentials.users.contains{ $0.username == username && $0.password == password }
+    }
+}
